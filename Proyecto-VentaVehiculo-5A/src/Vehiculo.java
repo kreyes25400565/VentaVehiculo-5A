785482ab5499375b87ae0e9ed503e7c1681475ce
kreyes25400565 <kimoreyessa@ittepic.edu.mx>
2026-02-17 19:02:22 -0700

    Feat: Clase Vehiculo completa

--- Proyecto-VentaVehiculo-5A/src/Vehiculo.java
+++ Proyecto-VentaVehiculo-5A/src/Vehiculo.java
@@ -9,20 +9,65 @@
  */
 public class Vehiculo {
     private String marca;
+    private String modelo;
+    private int anio;
     private double precio;
-    private String motor;
+    private double capacidadCarga;
     
-    public Vehiculo (String marca, double precio, String motor){
+    public Vehiculo (String marca,String modelo, int anio, double precio, double capacidadCarga){
         this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
         this.precio = precio;
-        this.motor = motor;
+        this.capacidadCarga = capacidadCarga;
+    }
+    
+    //getters
+    public String getMarca(){
+        return marca;
+    }
+    public String getModelo(){
+        return modelo;
+    }
+    public int getAnio(){
+        return anio;
+    }
+    public double getPrecio(){
+        return precio;
+    }
+    public double getCapacidadCarga(){
+        return capacidadCarga;
+    }
+    
+    //setters
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    public void setAnio(int anio){
+        this.anio = anio;
+    }
+    public void setPrecio(double precio){
+        this.precio = precio;
+    }
+    public void setCapacidadCarga(double capacidadCarga){
+        this.capacidadCarga = capacidadCarga;
+    }
+    
+    public void aplicarDescuento(){
+        
     }
     
     @Override
     public String toString(){
-        return "Marca = "+this.marca+"\n"+
-                "Precio = "+this.precio+"\n"+
-                "Motor = "+this.motor+"\n";
+        return "Vehículo" +
+               "\nMarca: " + marca +
+               "\nModelo: " + modelo +
+               "\nAño: " + anio +
+               "\nPrecio: $" + precio +
+               "\nCapacidad de carga: " + capacidadCarga + " kg";
     }
     
 }
